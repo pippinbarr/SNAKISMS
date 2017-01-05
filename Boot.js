e335bfc27831866e7015adb0e72cbbd56601f536
Pippin Barr <pippin.barr@gmail.com>
2017-01-05 15:15:49 -0500

    Changed aspect ratio to be portrait, better for phone

--- Boot.js
+++ Boot.js
@@ -19,14 +19,16 @@ BasicGame.Boot.prototype = {
             //  If you have any desktop specific settings, they can go in here
             this.scale.pageAlignHorizontally = true;
             this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-            this.scale.setMinMax(480, 260, 800, 600);
+            // this.scale.setMinMax(480, 260, 800, 600);
+            this.scale.setMinMax(260, 480, 600, 800);
         }
         else
         {
             //  Same goes for mobile settings.
             //  In this case we're saying "scale the game, no lower than 480x260 and no higher than 1024x768"
             this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-            this.scale.setMinMax(480, 260, 1024, 768);
+            // this.scale.setMinMax(480, 260, 1024, 768);
+            this.scale.setMinMax(260, 480, 768, 1024);
             this.scale.forceLandscape = true;
             this.scale.pageAlignHorizontally = true;
         }
