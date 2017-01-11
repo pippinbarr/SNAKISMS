6e2c8c2b2d883fbdf08e35de556684b7d7bb9806
Pippin Barr <pippin.barr@gmail.com>
2017-01-11 12:35:01 -0500

    Implemented basic body eating with rest of the snake stopping in place and edible

--- snakes/Monism.js
+++ snakes/Monism.js
@@ -7,6 +7,8 @@ BasicGame.Monism.prototype.constructor = BasicGame.Snake;
 
 BasicGame.Monism.prototype.create = function () {
   BasicGame.Snake.prototype.create.call(this);
+
+  this.snakeStaticBitsGroup = this.game.add.group();
 };
 
 BasicGame.Monism.prototype.update = function () {
@@ -23,9 +25,7 @@ BasicGame.Monism.prototype.checkEdibleCollision = function () {
     if (this.snakeHead.x + this.GRID_SIZE/2 == edible.x && this.snakeHead.y == edible.y) {
       if (edible.text != '') {
         this.textGroup.remove(edible);
-        this.snakeBitsToAdd = this.NEW_BODY_PIECES_PER_APPLE;
-        this.score += this.APPLE_SCORE;
-        this.setScoreText(this.score.toString());
+        this.eat();
       }
     }
   },this);
@@ -33,14 +33,46 @@ BasicGame.Monism.prototype.checkEdibleCollision = function () {
   this.wallGroup.forEach(function (edible) {
     if (this.snakeHead.position.equals(edible.position)) {
       this.wallGroup.remove(edible);
-      this.snakeBitsToAdd = this.NEW_BODY_PIECES_PER_APPLE;
-      this.score += this.APPLE_SCORE;
-      this.setScoreText(this.score.toString());
+      this.eat();
     }
   },this);
 
+  this.snakeStaticBitsGroup.forEach(function (bit) {
+    if (this.snakeHead.position.equals(bit.position)) {
+      this.snakeStaticBitsGroup.remove(bit);
+      this.eat();
+    }
+  },this);
+
+  for (var i = 0; i < this.snake.length - 1; i++) {
+    if (this.snakeHead.position.equals(this.snake[i].position)) {
+      this.snakeBodyGroup.remove(this.snake[i]);
+      // Need to add this to a "randomly reposition timer"
+      var remove = this.snake.slice(0,i);
+      this.snake = this.snake.slice(i+1);
+      for (var j = 0; j < remove.length; j++) {
+        var bodyBit = remove[j];
+        this.snakeBodyGroup.remove(bodyBit);
+        this.snakeStaticBitsGroup.add(bodyBit);
+      }
+      this.eat();
+      break;
+    }
+  }
+
 
 };
+
+BasicGame.Monism.prototype.eat = function () {
+  this.snakeBitsToAdd = this.NEW_BODY_PIECES_PER_APPLE;
+  this.score += this.APPLE_SCORE;
+  this.setScoreText(this.score.toString());
+}
+
+BasicGame.Monism.prototype.checkBodyCollision = function () {
+  // No death on body
+};
+
 
 BasicGame.Monism.prototype.checkWallCollision = function () {
   // No death on wall
